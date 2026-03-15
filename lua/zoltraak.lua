eaf8661537882c336209586029864c07f796cb02
Ｔ・Ｋ <karuma.reason@gmail.com>
2026-03-15 11:05:58 +0900

    Rename.

--- lua/zoltraak.lua
+++ lua/zoltraak.lua
@@ -15,7 +15,7 @@ local default_config = {
 
 local function load_module(module_name)
   local ok, module = pcall(require, module_name)
-  assert(ok, string.format('psycho dependency error: %s not installed', module_name))
+  assert(ok, string.format('zoltraak dependency error: %s not installed', module_name))
   return module
 end
 
